e2752ac626b341c9daa3a913fff5991d35c1887d
Manik Garg <manikgarg06@gmail.com>
2020-10-18 01:22:03 +0530

    Added Basic

--- src/App.js
+++ src/App.js
@@ -9,23 +9,24 @@ function App() {
   const fenceRef = useRef();
 
   const addBoxHandler = () => {
-
-    setBoxes(prevBoxes => prevBoxes.concat({ id: Math.random(), zIndex: counter + 1 }))
-    setCounter(prevCounter => prevCounter + 1);
+    if(fenceRef.current.style.left < 1170){
+      setBoxes(prevBoxes => prevBoxes.concat({ id: Math.random(), zIndex: counter + 1 }))
+      setCounter(prevCounter => prevCounter + 1);
+    }
   }
   const handleSelectedBox = (box) => {
     if (selectedBox) {
-      if(selectedBox.id === box.id){
-        document.getElementById(box.id).style.backgroundColor='white';
+      if (selectedBox.id === box.id) {
+        document.getElementById(box.id).style.backgroundColor = 'white';
         setSelectedBox(undefined);
-      }else{
-        document.getElementById(box.id).style.backgroundColor='grey';
-        document.getElementById(selectedBox.id).style.backgroundColor='white';
+      } else {
+        document.getElementById(box.id).style.backgroundColor = 'grey';
+        document.getElementById(selectedBox.id).style.backgroundColor = 'white';
         setSelectedBox({ id: box.id });
       }
     } else {
       setSelectedBox({ id: box.id });
-      document.getElementById(box.id).style.backgroundColor='grey';
+      document.getElementById(box.id).style.backgroundColor = 'grey';
     }
   }
   useEffect(() => {
@@ -38,16 +39,25 @@ function App() {
       const element = document.getElementById(selectedBox.id);
       switch (event.keyCode) {
         case 83://S
-          element.style.top = `${fenceRef.current ? parseInt(element.style.top) + 5 : 2}px`;
+          element.style.top = `${parseInt(element.style.top) < fenceRef.current.clientHeight-90 ?
+            parseInt(element.style.top) + 5 : parseInt(element.style.top)}px`;
           break;
         case 87: //W
-          element.style.top = `${fenceRef.current ? parseInt(element.style.top) - 5 : 2}px`;
+          element.style.top = `${fenceRef.current.offsetTop < parseInt(element.style.top) ?
+            parseInt(element.style.top) - 5 : parseInt(element.style.top)}px`;
           break;
         case 65: //A
-          element.style.left = `${fenceRef.current ? parseInt(element.style.left) - 5 : 2}px`;
+          element.style.left = `${fenceRef.current.offsetLeft < parseInt(element.style.left) ?
+            parseInt(element.style.left) - 5 : parseInt(element.style.left)}px`;
           break;
         case 68: // D
-          element.style.left = `${fenceRef.current ? parseInt(element.style.left) + 5 : 2}px`;
+          element.style.left = `${parseInt(element.style.left) < fenceRef.current.clientWidth ?
+            parseInt(element.style.left) + 5 : parseInt(element.style.left)}px`;
+          break;
+        case 46: // D
+          const newBoxes = boxes.filter(box => box.id !== selectedBox.id);
+          setBoxes(newBoxes);
+          setSelectedBox(undefined);
           break;
         default:
           break;
@@ -56,14 +66,14 @@ function App() {
     // console.log(event.keyCode)
     // console.log(event.key)
   }
-  // console.log(fenceRef)
+  console.log(fenceRef)
   return (
     <div className="App-header">
       <div className="Fence" ref={fenceRef} >
         {boxes.map(box => {
           return (<div className="Box" id={box.id} onClick={() => handleSelectedBox(box)} style={{
             backgroundColor: 'white', zIndex: box.zIndex, top: fenceRef.current.offsetTop + 5 * (box.zIndex - 1),
-            left: fenceRef.current.offsetLeft + 10 + 30 * (box.zIndex - 1)
+            left: fenceRef.current.offsetLeft + 30 * (box.zIndex - 1)
           }} key={box.id}>
           </div>)
         })}
